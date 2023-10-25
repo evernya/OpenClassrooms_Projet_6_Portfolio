758da238272f79dd0d5b383bfee2c28667702a12
EmmaCYC <emmanuelle.cyc@live.fr>
2023-10-25 15:46:22 +0200

    fichier login js terminé

--- FrontEnd/javascript/login.js
+++ FrontEnd/javascript/login.js
@@ -1,22 +1,24 @@
 
-let user = {
-    "email" : "sophie.bluel@test.tld",
-    "mdp" : "S0phie",
-}
 
 function seConnecter() {
     let connection = document.querySelector(".formConnection");
     connection.addEventListener("submit", function (event) {
         event.preventDefault();
 
+        let user = {
+            "email" : "sophie.bluel@test.tld",
+            "password" : "S0phie",
+        };
+
         //création de l'objet de connection
         let stringConnection = {
             email: event.target.querySelector("[name=email]").value,
-            motdepasse: event.target.querySelector("[name=mdp]").value,
+            password: event.target.querySelector("[name=mdp]").value,
         };
 
         //création de la charge utile au format JSO?
         const chargeUtile = JSON.stringify(stringConnection);
+        console.log(chargeUtile)
 
         fetch("http://localhost:5678/api/users/login", {
 
@@ -29,11 +31,36 @@ function seConnecter() {
             return response.json();
         })
         .then(function (result){
-            if(user.email === stringConnection.email && user.mdp === stringConnection.mdp){
-                console.log("Test ok")
+            if(result.token){
+
+                //stocker les données dans le local storage
+                localStorage.setItem("token", result.token);
+
+                setTimeout(() => {
+                                window.location.href = "index.html"
+                            }, 1000)
             } else {
-                console.log("Test pas ok")
+                alert("Erreur dans l'identifiant ou le mot de passe.")
             }
         })
+        .catch((error) => {
+            // Gérer les erreurs
+            console.error("Erreur lors de la récupération des informations:", error);
+        })
+
+        // .then(function (result){
+        //     if(user.email === stringConnection.email && user.password === stringConnection.password){
+        //         setTimeout(() => {
+        //             window.location.href = "index.html"
+        //         }, 1000)
+                
+        //     } else {
+        //         setTimeout(() => {
+        //             window.location.href = "login.html"
+        //         }, 1000)
+        //     }
+        // })
     })
 }
+
+seConnecter();
